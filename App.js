ebe63c5495222b6791df069ec8d543a71da1ead8
Albeniz <javier_albeniz@hotmail.com>
2018-06-26 17:48:55 +0200

    Drug Price changes sucessfully. Also initial drug quantities and if quantity is 0, drug doesnt render

--- App.js
+++ App.js
@@ -10,157 +10,321 @@ export default class App extends Component {
       {
         name: 'Cocaine',
         icon: require('./Images/cocaine.png'),
-        currentPrice: 20,
-        priceRise: false,
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
         greenArrow: require('./Images/greenArrow.png'),
         redArrow: require('./Images/redArrow.png')
-      }, {
+      },
+      {
         name: 'Crack',
         icon: require('./Images/crack.png'),
-        currentPrice: 30,
-        priceRise: false,
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
         greenArrow: require('./Images/greenArrow.png'),
         redArrow: require('./Images/redArrow.png')
-      }, {
+      },
+      {
         name: 'Ecstasy',
         icon: require('./Images/ecstasy.png'),
-        currentPrice: 17,
-        priceRise: false,
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
         greenArrow: require('./Images/greenArrow.png'),
         redArrow: require('./Images/redArrow.png')
-      }
+      },
+      {
+        name: 'Hashish',
+        icon: require('./Images/hashish.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
+      {
+        name: 'Heroin',
+        icon: require('./Images/heroin.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
+      {
+        name: 'Ice',
+        icon: require('./Images/ice.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
+      {
+        name: 'Kat',
+        icon: require('./Images/kat.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
+      {
+        name: 'LSD',
+        icon: require('./Images/lsd.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
+      {
+        name: 'MDA',
+        icon: require('./Images/mda.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
+      {
+        name: 'Morphine',
+        icon: require('./Images/morphine.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
+      {
+        name: 'Mushrooms',
+        icon: require('./Images/mushrooms.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
+      {
+        name: 'Opium',
+        icon: require('./Images/opium.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
+      {
+        name: 'PCP',
+        icon: require('./Images/pcp.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
+      {
+        name: 'Peyote',
+        icon: require('./Images/peyote.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
+      {
+        name: 'Special K',
+        icon: require('./Images/specialk.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
+      {
+        name: 'Speed',
+        icon: require('./Images/speed.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
+      {
+        name: 'Weed',
+        icon: require('./Images/cocaine.png'),
+        price: 5,
+        priceVariation: 2,
+        minPrice:0,
+        maxPrice:10,
+        priceRise: '',
+        quantity:0,
+        greenArrow: require('./Images/greenArrow.png'),
+        redArrow: require('./Images/redArrow.png')
+      },
     ]
   }
 
+  componentWillMount(){
+    this.setInitialDrugQuantity()
+  }
+
   changePrice() {
-    console.log('asdasdasd')
+    console.log('*******************Changing Prices*******************')
     this.setState((previousState) => {
       previousState.drugList.forEach((drug) => {
-        drug.currentPrice++
+
+        if(drug.quantity != 0){
+
+          var addOrRemove = Math.floor(Math.random() * 2) + 1
+          console.log(addOrRemove)
+          switch (addOrRemove) {
+
+            case 1:
+
+            var randomNumber = Math.floor(Math.random() * drug.priceVariation) + 1
+            console.log(drug.name + ' price increases in: ' + randomNumber + '$')
+            if((drug.price + randomNumber) < drug.maxPrice){
+              drug.price = drug.price + drug.priceVariation
+              drug.priceRise = true
+            }else{
+              console.log('price is too high')
+              drug.priceRise = ''
+            }
+              break;
+
+            case 2:
+
+            var randomNumber = Math.floor(Math.random() * drug.priceVariation) + 1
+            console.log(drug.name + ' price decreases in: ' + randomNumber + '$')
+            if((drug.price - randomNumber) > drug.minPrice){
+              drug.price = drug.price - drug.priceVariation
+              drug.priceRise = false
+            }else{
+              console.log('price is too low!')
+              drug.priceRise = ''
+            }
+
+              break;
+          }
+        }
+      })
+
+      return previousState
+    })
+  }
+
+  setInitialDrugQuantity(){
+    console.log('*******************Changing Quantities*******************')
+    this.setState((previousState) => {
+      previousState.drugList.forEach((drug) => {
+        var randomNumber = Math.floor(Math.random() * 2) + 1
+        if(randomNumber != 2){
+          drug.quantity = 25
+        }
       })
       return previousState
     })
   }
-renderDrugList() {
-  return this.state.drugList.map(function(drug, index) {
-    return <Drug key={index} drug={drug} onPress={() => console.log('You pressed: ' + drug.name)}/>
-  }.bind(this))
-}
 
-render() {
-  return (<View style={styles.mainView}>
+  renderDrugList() {
+    return this.state.drugList.map(function(drug, index) {
+      if(drug.quantity != 0){
+        return <Drug key={index} drug={drug} onPress={() => console.log('You pressed: ' + drug.name)}/>
+      }
+    }.bind(this))
+  }
 
-    <ScrollView style={styles.listView}>
-      {this.renderDrugList()}
-    </ScrollView>
+  render() {
+    return (<View style={styles.mainView}>
 
-    <View style={styles.bottomMenuView}>
-      <Swiper showsButtons={false}>
+      <ScrollView style={styles.listView}>
+        {this.renderDrugList()}
+      </ScrollView>
 
+      <View style={styles.bottomMenuView}>
         <View>
           <TouchableOpacity onPress={() => this.changePrice()}>
             <Text>Explore</Text>
           </TouchableOpacity>
         </View>
+      </View>
 
-        <View>
-          <Text>Explore</Text>
-        </View>
-
-      </Swiper>
-    </View>
-
-  </View>)
-}
+    </View>)
+  }
 }
 
 const styles = {
-mainView: {
-  flex: 1,
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  marginTop: 35
-},
-listView: {
-  backgroundColor: 'skyblue',
-  width: '100%',
-  height: '80%'
-},
-bottomMenuView: {
-  backgroundColor: 'steelblue',
-  width: '100%',
-  height: '20%'
-},
-swiperSlide: {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: '#fff'
+  mainView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 35
+  },
+  listView: {
+    backgroundColor: 'skyblue',
+    width: '100%',
+    height: '80%'
+  },
+  bottomMenuView: {
+    backgroundColor: 'steelblue',
+    width: '100%',
+    height: '20%'
+  },
+  swiperSlide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff'
+  }
 }
-}
-
-// {
-//   name:'Crack',
-//   icon:require('./Images/crack.png')
-// },
-// {
-//   name:'Ecstasy',
-//   icon:require('./Images/ecstasy.png')
-// },
-// {
-//   name:'Hashish',
-//   icon:require('./Images/hashish.png')
-// },
-// {
-//   name:'Heroin',
-//   icon:require('./Images/heroin.png')
-// },
-// {
-//   name:'Ice',
-//   icon:require('./Images/ice.png')
-// },
-// {
-//   name:'Kat',
-//   icon:require('./Images/kat.png')
-// },
-// {
-//   name:'LSD',
-//   icon:require('./Images/lsd.png')
-// },
-// {
-//   name:'MDA',
-//   icon:require('./Images/mda.png')
-// },
-// {
-//   name:'Morphine',
-//   icon:require('./Images/morphine.png')
-// },
-// {
-//   name:'Mushrooms',
-//   icon:require('./Images/mushrooms.png')
-// },
-// {
-//   name:'Opium',
-//   icon:require('./Images/opium.png')
-// },
-// {
-//   name:'PCP',
-//   icon:require('./Images/pcp.png')
-// },
-// {
-//   name:'Peyote',
-//   icon:require('./Images/peyote.png')
-// },
-// {
-//   name:'Special-K',
-//   icon:require('./Images/specialk.png')
-// },
-// {
-//   name:'Speed',
-//   icon:require('./Images/speed.png')
-// },
-// {
-//   name:'Cannabis',
-//   icon:require('./Images/weed.png')
-// }
--- Components/Common/Drug.js
+++ Components/Common/Drug.js
@@ -7,64 +7,122 @@ export default class Drug extends Component {
     super(props)
   }
 
-  changePrice(){
-
+  checkPrice() {
+    if (this.props.drug.priceRise === false) {
+      console.log(this.props.drug.name + ' is -')
+      return <View style={styles.arrowView}><Image style={styles.arrowImageStyle} source={this.props.drug.redArrow}/></View>
+    }
+    if(this.props.drug.priceRise === true){
+      console.log(this.props.drug.name + ' is +')
+      return <View style={styles.arrowView}><Image style={styles.arrowImageStyle} source={this.props.drug.greenArrow}/></View>
+    }else {
+      console.log(this.props.drug.name + ' is the same')
+      return <View style={styles.arrowView}><Image style={styles.arrowImageStyle} source={require('../../Images/empty.png')}/></View>
+    }
   }
 
-  checkPrice() {
-    if (!this.props.drug.priceRise) {
-      console.log('TRUE ' + this.props.drug.priceRise)
-      return <Image style={styles.arrowImageStyle} source={this.props.drug.redArrow}/>
-    } else {
-      console.log('FALSE ' + this.props.drug.priceRise)
-      return <Image style={styles.arrowImageStyle} source={this.props.drug.greenArrow}/>
+  renderDrugList(){
+    if(this.props.drug.quantity !== 0){
+      return(
+        <TouchableOpacity style={styles.touchableStyle} onPress={this.props.onPress}>
+          <LinearGradient style={styles.linearGradient} colors={['#f7f7f7', '#fff']}>
+            <View style={styles.imageView}>
+              <Image style={styles.imageStyle} source={this.props.drug.icon}/>
+            </View>
+            <View style={styles.nameView}>
+              <Text style={styles.textStyle}>{this.props.drug.name}</Text>
+            </View>
+            <View style={styles.quantityView}>
+              <Text style={styles.textStyle}>{this.props.drug.quantity}</Text>
+            </View>
+            <View style={styles.priceView}>
+              <Text style={styles.priceStyle}>{this.props.drug.price}</Text>
+            </View>
+            {this.checkPrice()}
+          </LinearGradient>
+        </TouchableOpacity>
+      )
     }
   }
 
   render() {
-    return (<TouchableOpacity style={styles.touchableStyle} onPress={this.props.onPress}>
-      <LinearGradient style={styles.linearGradient} colors={['#f7f7f7', '#fff']}>
-        <Image style={styles.imageStyle} source={this.props.drug.icon}/>
-        <Text style={styles.textStyle}>{this.props.drug.name}</Text>
-        <Text style={styles.priceStyle}>{this.props.drug.currentPrice}</Text>
-        {this.checkPrice()}
-      </LinearGradient>
-    </TouchableOpacity>)
+    return (this.renderDrugList())
   }
 }
 
 const styles = {
-  priceStyle: {
-    alignSelf: 'center',
-    fontSize: 18,
-    marginLeft: 50,
-    color: '#515151'
-  },
   touchableStyle: {
     alignSelf: 'stretch',
     alignItems: 'center',
+    justifyContent: 'flex-start',
     backgroundColor: '#fff',
     borderWidth: 0.5,
     borderColor: '#d6d6d6',
-    height: 50
+    height: 45
+  },
+  imageView:{
+    // backgroundColor:'blueviolet',
+    height:'100%',
+    width:'15%',
+    alignItems:'center',
+    flexDirection:'row',
+    justifyContent:'center'
   },
   imageStyle: {
-    width: 30,
-    alignSelf: 'center',
-    height: 30,
-    marginLeft: 10
-  },
-  arrowImageStyle: {
     width: 25,
     alignSelf: 'center',
     height: 25,
-    marginLeft: 80
+  },
+  nameView:{
+    // backgroundColor:'steelblue',
+    height:'100%',
+    width:'35%',
+    alignItems:'center',
+    justifyContent:'center',
+    flexDirection:'row',
+    justifyContent:'flex-start'
+  },
+  quantityView:{
+    // backgroundColor:'powderblue',
+    height:'100%',
+    width:'15%',
+    alignItems:'center',
+    justifyContent:'center',
+    flexDirection:'row',
+    justifyContent:'flex-start'
   },
   textStyle: {
     alignSelf: 'center',
-    fontSize: 18,
-    marginLeft: 20,
-    color: '#515151'
+    fontSize: 16,
+    color: '#515151',
+  },
+  priceView:{
+    // backgroundColor:'cornflowerblue',
+    height:'100%',
+    width:'20%',
+    alignItems:'center',
+    justifyContent:'center',
+    flexDirection:'row',
+    justifyContent:'flex-start'
+  },
+  priceStyle: {
+    alignSelf: 'center',
+    fontSize: 16,
+    color: '#515151',
+  },
+  arrowView:{
+    // backgroundColor:'blanchedalmond',
+    height:'100%',
+    width:'15%',
+    alignItems:'center',
+    justifyContent:'center',
+    flexDirection:'row',
+    justifyContent:'center'
+  },
+  arrowImageStyle: {
+    width: 20,
+    alignSelf: 'center',
+    height: 20
   },
   linearGradient: {
     width: '100%',
